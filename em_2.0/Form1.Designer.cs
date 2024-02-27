43e95a6396f31a6db29c4930b7b2c758710e4622
TocsaXawer <csabaeror@gmail.com>
2024-02-27 19:09:33 +0100

    Add project files.

---
+++ em_2.0/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace em_2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.setting = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.Start);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.setting);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.panel1.Name = "panel1";
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Start
+            // 
+            resources.ApplyResources(this.Start, "Start");
+            this.Start.BackColor = System.Drawing.Color.White;
+            this.Start.Name = "Start";
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // setting
+            // 
+            resources.ApplyResources(this.setting, "setting");
+            this.setting.Name = "setting";
+            this.setting.UseVisualStyleBackColor = true;
+            this.setting.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Opacity = 0.95D;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button setting;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ em_2.0/Form1.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+
+
+namespace em_2._0
+{
+    
+    public partial class Form1 : Form
+    {
+        class ConfigData
+        {
+            public string path { get; set; }
+            public string extension { get; set; }
+
+            public string output_folder { get; set; }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        static List<string> SaveToPST(string[] filepaths, string outputPath)
+        {
+            List<string> processedFiles = new List<string>();
+
+            Outlook.Application outlookApp = new Outlook.Application();
+            Outlook.NameSpace outlookNamespace = outlookApp.GetNamespace("MAPI");
+            Outlook.Folder rootFolder = outlookNamespace.Session.DefaultStore.GetRootFolder() as Outlook.Folder;
+
+
+            string filePath = @"C:\em\.config.json";
+            string directory = "";
+            if (File.Exists(filePath))
+            {  // Olvassa be a JSON tartalmat a fájlból
+                string jsonContent = File.ReadAllText(filePath);
+
+                // Deszerializálja a JSON-t egy objektummá
+                ConfigData configData = JsonConvert.DeserializeObject<ConfigData>(jsonContent);
+
+                // Most már használhatod a configData objektumot, amely tartalmazza a JSON-ből kiolvasott adatokat
+
+                directory = configData.output_folder;
+            }
+            else
+            {
+                MessageBox.Show("Ismeretlen Hiba");
+            }
+
+                // Ellenőrizzük, hogy a célmappa létezik-e, ha nem, létrehozzuk
+                Outlook.Folder targetFolder;
+            try
+            {
+                targetFolder = rootFolder.Folders[directory] as Outlook.Folder;
+            }
+            catch (System.Exception)
+            {
+                targetFolder = rootFolder.Folders.Add(directory, Outlook.OlDefaultFolders.olFolderInbox) as Outlook.Folder;
+            }
+
+            // Fájlok feldolgozása és küldése
+            for (int i = 0; i < filepaths.Length; i++)
+            {
+                // Az adatok hozzáadása a processedFiles listához
+                processedFiles.Add(filepaths[i]);
+
+                Outlook.MailItem mailItem = outlookApp.CreateItem(Outlook.OlItemType.olMailItem) as Outlook.MailItem;
+                mailItem.Subject = Path.GetFileName(filepaths[i]);
+                mailItem.Body = "This is the attached file: " + Path.GetFileName(filepaths[i]);
+                mailItem.Attachments.Add(filepaths[i]);
+                mailItem.Save();
+                mailItem.Move(targetFolder);
+            }
+
+            outlookNamespace.Logoff();
+
+            // Visszaadjuk a feldolgozott fájlneveket
+            return processedFiles;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\em\.config.json";
+
+            if (File.Exists(filePath))
+            {
+                // Olvassa be a JSON tartalmat a fájlból
+                string jsonContent = File.ReadAllText(filePath);
+
+                // Deszerializálja a JSON-t egy objektummá
+                ConfigData configData = JsonConvert.DeserializeObject<ConfigData>(jsonContent);
+
+                // Most már használhatod a configData objektumot, amely tartalmazza a JSON-ből kiolvasott adatokat
+                MessageBox.Show($"A config file tartalma:\npath: {configData.path}\nKiterjesztés: {configData.extension}\nMentési mappa: {configData.output_folder}");
+            }
+            else
+            {
+                MessageBox.Show("A fájl nem létezik: " + filePath);
+            }
+            
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\em\.config.json";
+            if (File.Exists(filePath))
+            {
+                // Olvassa be a JSON tartalmat a fájlból
+                string jsonContent = File.ReadAllText(filePath);
+
+                // Deszerializálja a JSON-t egy objektummá
+                ConfigData configData = JsonConvert.DeserializeObject<ConfigData>(jsonContent);
+
+                // Most már használhatod a configData objektumot, amely tartalmazza a JSON-ből kiolvasott adatokat
+
+                string directoryPath = configData.path;
+                string extension = configData.extension;
+                
+
+                string[] filepaths = Directory.GetFiles(directoryPath, "*" + extension, SearchOption.AllDirectories);
+
+                // A feldolgozott fájlnevek megjelenítése az textBox1 szövegében
+                foreach (string processedFile in SaveToPST(filepaths, "C:\\Output.pst"))
+                {
+                    textBox1.AppendText(processedFile + Environment.NewLine);
+                }
+            }
+            else
+            {
+                MessageBox.Show("A fájl nem létezik: " + filePath);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+
+            folderBrowserDialog1.Description = "Select a folder";
+            folderBrowserDialog1.RootFolder = Environment.SpecialFolder.MyComputer; // Indítási mappa beállítása
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = @"C:\em\.config.json";
+
+                if (File.Exists(filePath))
+                {
+                    // Olvassa be a JSON tartalmat a fájlból
+                    string jsonContent = File.ReadAllText(filePath);
+
+                    // Deszerializálja a JSON-t egy objektummá
+                    ConfigData configData = JsonConvert.DeserializeObject<ConfigData>(jsonContent);
+
+                    // Path módosítása
+
+                    // Ha a felhasználó kiválasztott egy mappát, itt kezelheted le, pl. megteheted vele, amit akarsz
+                    string newPath = folderBrowserDialog1.SelectedPath;
+                    configData.path = newPath;
+
+                    // A fájlba írás előtt JSON formátumra alakítjuk a configData objektumot
+                    string updatedJsonContent = JsonConvert.SerializeObject(configData, Formatting.Indented);
+
+                    // A fájlba írás
+                    File.WriteAllText(filePath, updatedJsonContent);
+
+                    MessageBox.Show($"Mappa sikeresen módosítva. Erre {newPath}");
+                }
+                else
+                {
+                    MessageBox.Show("A fájl nem létezik: " + filePath);
+                }
+                
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Létrehozunk egy szöveg beviteli mezőt
+            TextBox inputTextBox = new TextBox();
+            inputTextBox.Dock = DockStyle.Top;
+
+            // Létrehozunk egy párbeszédpanelt, amely tartalmazza a szöveg beviteli mezőt
+            Form inputForm = new Form();
+            inputForm.Text = "kiterjesztés pl:.mp4";
+            inputForm.Size = new System.Drawing.Size(180, 100);
+            inputForm.Controls.Add(inputTextBox);
+
+            // Létrehozunk egy gombot a párbeszédpanelen, hogy megerősíthessük a beírt szöveget
+            Button confirmButton = new Button();
+            confirmButton.Text = "OK";
+            confirmButton.Dock = DockStyle.Bottom;
+            confirmButton.Click += (confirmSender, confirmEventArgs) =>
+            {
+                string filePath = @"C:\em\.config.json";
+
+                if (File.Exists(filePath))
+                {
+                    // Olvassa be a JSON tartalmat a fájlból
+                    string jsonContent = File.ReadAllText(filePath);
+
+                    // Deszerializálja a JSON-t egy objektummá
+                    ConfigData configData = JsonConvert.DeserializeObject<ConfigData>(jsonContent);
+                    string newExtension = inputTextBox.Text;
+                    configData.extension = newExtension;
+
+                    // A fájlba írás előtt JSON formátumra alakítjuk a configData objektumot
+                    string updatedJsonContent = JsonConvert.SerializeObject(configData, Formatting.Indented);
+
+                    // A fájlba írás
+                    File.WriteAllText(filePath, updatedJsonContent);
+
+                    MessageBox.Show("Kiterjesztés módosítva.");
+                }
+                else
+                {
+                    Console.WriteLine("A fájl nem létezik: " + filePath);
+                }
+                inputForm.Close(); // Bezárjuk a párbeszédpanelt
+            };
+            inputForm.Controls.Add(confirmButton);
+
+            // Megjelenítjük a párbeszédpanelt modális módban, hogy ne lehessen más tevékenységet végezni
+            inputForm.ShowDialog();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            // Létrehozunk egy szöveg beviteli mezőt
+            TextBox inputTextBox = new TextBox();
+            inputTextBox.Dock = DockStyle.Top;
+
+            // Létrehozunk egy párbeszédpanelt, amely tartalmazza a szöveg beviteli mezőt
+            Form inputForm = new Form();
+            inputForm.Text = "kiterjesztés pl:.mp4";
+            inputForm.Size = new System.Drawing.Size(180, 100);
+            inputForm.Controls.Add(inputTextBox);
+
+            // Létrehozunk egy gombot a párbeszédpanelen, hogy megerősíthessük a beírt szöveget
+            Button confirmButton = new Button();
+            confirmButton.Text = "OK";
+            confirmButton.Dock = DockStyle.Bottom;
+            confirmButton.Click += (confirmSender, confirmEventArgs) =>
+            {
+                string filePath = @"C:\em\.config.json";
+
+                if (File.Exists(filePath))
+                {
+                    // Olvassa be a JSON tartalmat a fájlból
+                    string jsonContent = File.ReadAllText(filePath);
+
+                    // Deszerializálja a JSON-t egy objektummá
+                    ConfigData configData = JsonConvert.DeserializeObject<ConfigData>(jsonContent);
+                    string newExtension = inputTextBox.Text;
+                    configData.output_folder = newExtension;
+
+                    // A fájlba írás előtt JSON formátumra alakítjuk a configData objektumot
+                    string updatedJsonContent = JsonConvert.SerializeObject(configData, Formatting.Indented);
+
+                    // A fájlba írás
+                    File.WriteAllText(filePath, updatedJsonContent);
+
+                    MessageBox.Show("Mentési Mappa módosítva.");
+                }
+                else
+                {
+                    Console.WriteLine("A fájl nem létezik: " + filePath);
+                }
+                inputForm.Close(); // Bezárjuk a párbeszédpanelt
+            };
+            inputForm.Controls.Add(confirmButton);
+
+            // Megjelenítjük a párbeszédpanelt modális módban, hogy ne lehessen más tevékenységet végezni
+            inputForm.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string directoryPath = @"C:\em";
+            string filePath = @"C:\em\.config.json";
+
+            // Ellenőrizze, hogy létezik-e a könyvtár
+            if (!Directory.Exists(directoryPath))
+            {
+                // Ha nem létezik, hozza létre
+                Directory.CreateDirectory(directoryPath);
+                MessageBox.Show("Status: Config mappa inicializálás.");
+            }
+
+            // Ellenőrizze, hogy létezik-e a fájl
+            if (!File.Exists(filePath))
+            {
+                // Ha nem létezik, hozza létre
+                using (FileStream fs = File.Create(filePath))
+                    MessageBox.Show("Status: Config file configurálás.");
+            }
+            else
+            {
+                MessageBox.Show("Status: Üdv újra minden rendben van.\n\n");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
